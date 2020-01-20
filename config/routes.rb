5e84fd5941edcf1419bbf18b58f806a7944ae59d
ancolon2 <andrew.colon@yahoo.com>
2020-01-20 20:34:32 +0000

    Setup of root and login pages and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
+  root 'pages#home'
+  get 'login', to: 'sessions#new'
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
