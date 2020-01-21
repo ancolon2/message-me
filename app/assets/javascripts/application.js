e1e535a76aa3f3f67eb64be21297712e69d4e5cb
ancolon2 <andrew.colon@yahoo.com>
2020-01-21 06:03:01 +0000

    Added messages functionality via channel connection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,10 +17,22 @@
 //= require semantic-ui
 //= require_tree .
 
+scroll_bottom = function(){
+  if ($('#messages').length > 0){
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  };
+};
+
+submit_message = function(){
+  $('#message_body').target.value = "";
+};
+
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  submit_message();
+  scroll_bottom();
 })
 
