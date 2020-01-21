488e29e74ad52977ca99da6e9af9a965fa045ad5
ancolon2 <andrew.colon@yahoo.com>
2020-01-21 05:06:17 +0000

    Added message sending functionality and message error handling

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,13 +1,15 @@
 class ChatroomController < ApplicationController
+  before_action :require_user
   
   def index
+    @message = Message.new
     @messages = Message.all
-    @username = User.find(10)
   end
   
   def home
     
   end
+  
   def about
     
   end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show]
+  #before_action :require_same_user, only: [:edit, :update, :destroy]
+  #before_action :require_admin, only: [:destroy]
+  
+  def index
+    #@users = User.paginate(page: params[:page], per_page:5)
+  end
+  
+  def show
+    #@user = @messages.user.username
+    #@messages = Message.all
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    #debugger
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome to the alpha blog #{@user.username}"
+      redirect_to user_path(@user)
+    else
+      render 'new'  
+    end
+  end
+    
+  def edit
+  end
+      
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+end
