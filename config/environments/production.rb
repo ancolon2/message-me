84cabaa70221b3b08b85a1584f2b11e4c6b95b5f
ancolon2 <andrew.colon@yahoo.com>
2020-01-23 02:02:00 +0000

    Finished up MessageMeApp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,10 +42,10 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain
-  config.action_cable.mount_path = '/cable'
+  #config.action_cable.mount_path = '/cable'
   config.action_cable.url = 'wss://messagemeapplication.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'http://messagemeapplication.herokuapp.com', /http:\/\/messagemeapplication.*/ ]
-  config.web_socket_server_url = "wss://messagemeapplication.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://messagemeapplication.herokuapp.com/"
   #Rails.application.config.action_cable.allowed_request_origins = ['messagemeapplication.herokuapp.com']
  
 
